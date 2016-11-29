f9034b3375be827b02c9a9b09019d7e6cee4ada8
vlasn <vlasn@tlu.ee>
2016-11-29 22:51:30 +0200

    Implementing first version of login

--- api/request.php
+++ api/request.php
@@ -1,7 +1,11 @@
 <?php
 /**
- * splitting url
+ *
  */
+require('src/auth.class.php');
+require('../../dbdata.php');
+$PDO = new PDO("mysql:dbname=$dbname;host=$location",$user,$pass);
+$Auth = new auth($PDO);
 
 $params = array();
 $parts = explode('/', $_SERVER['REQUEST_URI']);
@@ -33,7 +37,12 @@ if(isset($_GET['apikey'])) {
     }
     if (isset($_GET['submit'])){
         if($_GET['submit']=='login'){
-            echo "loginiskript jookseb siin";
+            if(isset($_GET['phone'])&&isset($_GET['pass'])){
+                $response = $auth->login($_GET['phone'],$_GET['pass']);
+                print_r(json_encode($response));
+            }else{
+                echo "these are not the droids you're looking for";
+            }
         }
         if($_GET['submit']=='rateEmployee'){
             echo "hindamise script";
