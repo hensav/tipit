dcb6db317f0e697a21d0487949f33fc8715d6608
toomas Thetloff <thetloffline@users.noreply.github.com>
2016-11-30 00:30:49 +0200

    added signup_form, changed login

--- views/login.php
+++ views/login.php
@@ -8,25 +8,37 @@
 
 <div class="login">
     <div class="login_form">
-
         <form method="POST">
-            
-            <input class="login_field" type="email" placeholder="your e-mail" name="loginEmail" value="">
+            <input class="form_field" type="text" name="phone" value="">
             <br><br>
-            <input class="login_field" type="text" name="firstname" value="">
+            <input class="form_field" type="password" placeholder="verification code" name="auth">
             <br><br>
-            <input class="login_field" type="text" name="phone" value="">
+            <input class="form_button" type="submit" value="logi sisse">
+        </form>
+    </div>
+</div>
+
+
+<div class="signup">
+    <div class="signup_form">
+        <form method="POST">
+
+            <input class="form_field" type="email" placeholder="your e-mail" name="loginEmail" value="">
             <br><br>
-            <input class="login_field" type="text" name="lastname" value="">
+            <input class="form_field" type="text" name="firstname" value="">
             <br><br>
-            <input class="login_field" type="password" placeholder="verification code" name="auth">
+            <input class="form_field" type="text" name="lastname" value="">
             <br><br>
-            <input class="login_button" type="submit" value="logi sisse">
+            <input class="form_field" type="text" name="role" value="">
+            <br><br>
+            <input class="form_field" type="text" name="phone" value="">
+            <br><br>
+            <input class="form_field" type="password" placeholder="verification code" name="auth">
+            <br><br>
+            <input class="form_button" type="submit" value="logi sisse">
 
         </form>
     </div>
-
-</div>
 
 </body>
 </html>
