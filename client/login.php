872fd00b7513350957e741d292938a64bf43ecee
henrysavi <hensav@users.noreply.github.com>
2016-11-30 15:53:23 +0200

    added client

--- client/login.php
+++ client/login.php
@@ -1,1 +1,9 @@
-adasdasss
+$url = "http://http://naturaalmajand.us/tipit/api/request.php/";
+
+require("./class/clientAuth.class.php");
+
+$clientAuth = new clientAuth;
+$user = "veljo@naturaalmajand.us";
+$pass = "parool";
+$result = $clientAuth -> loginRequest($url,$user,$pass);
+var_dump($result);
