217885b873be9439b6efb3b1833803f2b7f3c2b3
henrysavi <hensav@users.noreply.github.com>
2016-12-01 15:27:28 +0200

    added employee rating class query

--- api/src/employeeRating.class.php
+++ api/src/employeeRating.class.php
@@ -15,21 +15,22 @@ class employeeRating
         $this->conn = $PDO;
     }
 
-    function leaveRating($client_id, $employee_id, $main_score, $param2_score, $param3_score, $submitted){
+    function leaveRating($client_id, $employee_id, $main_score, $param2_score, $param3_score){
         $stmt = $this->conn->prepare(
-        //query
-
-        "
-            
-            ");
-
-        //query
+            "INSERT INTO ratings(client_id, employee_id, main_score, param2_score, param3_score, submitted)
+            VALUES(:client_id, :employee_id, :main_score, :param2_score, :param3_score, CURRENT_TIMESTAMP)"
+            );
+        if(empty($param2_score)){
+            $param2_score=null;
+        }
+        if(empty($param3_score)){
+            $param3_score=null;
+        }
         $stmt->bindParam(':client_id',$client_id,PDO::PARAM_INT);
         $stmt->bindParam(':employee_id',$employee_id,PDO::PARAM_INT);
         $stmt->bindParam(':main_score',$main_score,PDO::PARAM_INT);
         $stmt->bindParam(':param2_score',$param2_score,PDO::PARAM_INT);
         $stmt->bindParam(':param3_score',$param3_score,PDO::PARAM_INT);
-        $stmt->bindParam(':submitted',$submitted,PDO::PARAM_INT);
         $stmt->execute();
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
         return $result;
