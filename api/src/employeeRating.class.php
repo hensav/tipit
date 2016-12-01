b22db8227fb1b1800f01721aa96ea5bc35d409e4
henrysavi <hensav@users.noreply.github.com>
2016-12-01 15:12:35 +0200

    updated employee rating class

--- api/src/employeeRating.class.php
+++ api/src/employeeRating.class.php
@@ -17,5 +17,19 @@ class employeeRating
 
     function leaveRating($client_id, $employee_id, $main_score, $param2_score, $param3_score, $submitted){
         $stmt = $this->conn->prepare(
+            "
+            
+            ");
+
+
+        $stmt->bindParam(':client_id',$client_id,PDO::PARAM_INT);
+        $stmt->bindParam(':employee_id',$employee_id,PDO::PARAM_INT);
+        $stmt->bindParam(':main_score',$main_score,PDO::PARAM_INT);
+        $stmt->bindParam(':param2_score',$param2_score,PDO::PARAM_INT);
+        $stmt->bindParam(':param3_score',$param3_score,PDO::PARAM_INT);
+        $stmt->bindParam(':submitted',$submitted,PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result;
 
 }
