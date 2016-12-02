80bde5fcea895a766bdb67568b76383e60321188
henrysavi <hensav@users.noreply.github.com>
2016-12-02 16:36:18 +0200

    login form working with api

--- views/login.php
+++ views/login.php
@@ -43,67 +43,18 @@ if (isset($_POST["loginPassword"])) {
     }
 }
 
-
-
-// LOGIN EMAIL
-if (isset($_POST["loginEmail"])) {
-    if (empty($_POST["loginEmail"])) {
-        $loginEmailError = "insert e-mail";
-    }
-}
-
-if(isset($_POST["loginPassword"])){
-    if(empty($_POST["loginPassword"])){
-        $loginPasswordError="insert password";
-    }
-}
-
-// SIGNUP EMAIL
-if (isset($_POST["signupEmail"])) {
-    if (empty ($_POST["signupEmail"])) {
-        $signupEmailError = " field is required";
+//checking if loginEmail and loginPassword have been posted
+if (isset($_POST["loginEmail"]) && isset($_POST["loginPassword"])){
+//replacing empty (non-obligatory) fields with empty strings to avoid api url bugs
+    if(empty($_POST['phone'])){
+        $phone='';
     } else {
-        $signupEmail = $_POST["signupEmail"];
+        $phone=$_POST['phone'];
     }
-}
 
-if (isset($_POST["signupPassword"])) {
-    if (empty ($_POST["signupPassword"])) {
-        $signupPasswordError = " field is required";
-    } else {
-        $signupPassword = $_POST["signupPassword"];
-    }
-}
-
-// Kontrollin, kas signupEmailError ja signupPasswordError on "" ehk e-post ja parool on sisestatud
-if ($signupEmailError == "" &&
-    $signupPasswordError == "" &&
-    isset($_POST["signupEmail"]) &&
-    isset($_POST["signupPassword"])
-) {
-//see osa on vajalik vaid testimiseks, kas eelnev on õige
-    echo "Salvestan.. <br>";
-    echo "email: ".$signupEmail."<br>";
-    echo "parool: ".$_POST["signupPassword"]."<br>";
-// parool krüpteeritakse
-    $password = hash("sha512", $_POST["signupPassword"]);
-
-    echo $password."<br>";
-// signup FUNCTION
-    signup($signupEmail, $password);
-}
-
-$notice = "";
-//$defaultEmail = "sisesta_email";
-if (isset($_POST["loginEmail"]) &&
-    isset($_POST["loginPassword"]) &&
-    !empty($_POST["loginEmail"]) &&
-    !empty($_POST["loginPassword"])
-) {
-// login Autofill
-    $loginSavedEmail = $_POST["loginEmail"];
-//
-    $notice = login ($_POST["loginEmail"], $_POST["loginPassword"]);
+    //$result = $clientAuth -> loginRequest($url,$user,$pass);
+    $result = $clientAuth->loginRequest($url,$_POST['loginEmail'],$_POST['loginPassword'],$phone);
+    var_dump($result);
 }
 
 ?>
@@ -113,21 +64,39 @@ if (isset($_POST["loginEmail"]) &&
 <html>
 <head>
     <title>tipit dirty</title>
-</head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./main.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <script
+        src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous"></script></head>
 <body>
+<div class="wrapper">
 
 
-<div class="login">
 
-    <form method="POST" class="login form">
-        <input type = "email" placeholder="your e-mail" name="loginEmail" class="form__field" value="<?=$signupEmail;?>"> <?php echo $signupEmailError; ?>
-        <input type="text" placeholder="optional phone number" name="phone" value="" class="form__field field--optional">
-        <input type="password" placeholder="password" name="signupPassword" class="form__field"><?php echo $signupPasswordError; ?>
-        <input type="submit" value="login" class="form__button">
-    </form>
+    <div class="login">
 
-</div>
+        <form method="POST" class="login form">
+            <input type ="email" placeholder="your e-mail" name="loginEmail" class="form__field" value="<?=$loginEmail;?>">
+            <input type="text" placeholder="optional phone number" name="phone" value="" class="form__field field--optional">
+            <input type="password" placeholder="password" name="loginPassword" class="form__field">
+            <input type="submit" value="login" class="form__button">
+        </form>
+
+    </div>
 
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
