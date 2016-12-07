29fb4178ea47e52aeb50d2610c378de19f532d0d
henrysavi <hensav@users.noreply.github.com>
2016-12-07 16:32:31 +0200

    updated learn

--- views/learn.php
+++ views/learn.php
@@ -10,11 +10,11 @@
 
 <div class="wrapper">
     <div class="welcome">
-        <h1>Tipit!</h1>
+        <h1>What is<br>Tipit!</h1>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus ullamcorper eros ac pharetra. Proin placerat ante nec eros tincidunt, eget feugiat justo porta. In mattis mattis tortor. Cras ultrices posuere ex, vel sagittis tellus interdum quis. Nam ornare sodales convallis. Maecenas vestibulum nec justo in molestie. Maecenas ultricies tempus nisi id tempor. Nulla vitae mi libero. Morbi fermentum sapien eu ligula aliquet scelerisque. Duis aliquet purus est, ut rhoncus justo vestibulum id. Aliquam interdum dolor nec purus viverra, pharetra aliquam nisi vestibulum. Pellentesque eget nunc non est condimentum convallis.
         <br>        <br>
 
-        <a href="signup.php"><input type="button" value="Don't mind if I do" class="form__button"></a>
+        <a href="signup.php"><input type="button" value="Don't mind if I do" class="href__button"></a>
 
     </div>
 </div>
