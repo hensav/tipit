142205b38677ce77348ef9670ae52cf517f88591
henrysavi <hensav@users.noreply.github.com>
2016-12-07 19:18:49 +0200

    update

--- views/index.php
+++ views/index.php
@@ -3,18 +3,19 @@
 <html>
 <head>
     <title>tipit dirty</title>
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./main.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,900" rel="stylesheet">
 </head>
 <body>
 
 <div class="wrapper">
-	<div class="welcome">
-	<h1>Empower good people!</h1>
-        <a href="login.php"><input type="button" value="Don't mind if I do" class="form__button"></a>
-	    <a href="learn.php"><input type="button" value="learn more" class="form__button inactive--button"></a>
-	</div>
+    <div class="welcome">
+        <h1>Empower good people!</h1>
+        <a href="signup.php"><input type="button" value="Don't mind if I do" class="href__button"></a>
+        <a href="login.php"><input type="button" value="Login" class="href__button"></a>
+        <a href="learn.php"><input type="button" value="learn more" class="href__button inactive--button"></a>
+    </div>
 </div>
 
 </body>
