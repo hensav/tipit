ed5f9960aea0126c6820b1297355f799e8f15729
henrysavi <hensav@users.noreply.github.com>
2016-12-07 15:43:00 +0200

    fixed fukup

---
+++ views/learn.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Learn more</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./main.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="welcome">
+        <h1>Tipit!</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus ullamcorper eros ac pharetra. Proin placerat ante nec eros tincidunt, eget feugiat justo porta. In mattis mattis tortor. Cras ultrices posuere ex, vel sagittis tellus interdum quis. Nam ornare sodales convallis. Maecenas vestibulum nec justo in molestie. Maecenas ultricies tempus nisi id tempor. Nulla vitae mi libero. Morbi fermentum sapien eu ligula aliquet scelerisque. Duis aliquet purus est, ut rhoncus justo vestibulum id. Aliquam interdum dolor nec purus viverra, pharetra aliquam nisi vestibulum. Pellentesque eget nunc non est condimentum convallis.
+        <br>        <br>
+
+        <a href="signup.php"><input type="button" value="Don't mind if I do" class="form__button"></a>
+
+    </div>
+</div>
+
+</body>
+</html>
