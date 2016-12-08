6eb0fa6cfc1c70e46970aa3b161e02871324fb75
henrysavi <hensav@users.noreply.github.com>
2016-12-08 14:44:41 +0200

    added header

---
+++ views/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./main.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,900" rel="stylesheet">
+</head>
+
+<body>
+<div class="wrapper">
+    <div class="header">
+        Tip!t
+    </div>
+</div>
+
+
+</body>
+</html>
