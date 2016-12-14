aa4bed030a175b390e072eb902a39b849b8b43ea
thetloffline <toomasthetloff@Thetloffline.local>
2016-12-14 15:08:30 +0200

    added login

--- views/index.php
+++ views/index.php
@@ -4,10 +4,11 @@
     <div class="welcome">
         <h1>Empower good people!</h1>
         <a href="signup.php"><input type="button" value="Don't mind if I do" class="href__button"></a>
+        <a href="login.php"><input type="button" value="Login" class="href__button"></a> 
         <p><a href="learn.php">Learn more</a></p>
+       
        <!-- 
        <a href="learn.php"><input type="button" value="learn more" class="href__button inactive--button"></a>
-       <a href="login.php"><input type="button" value="Login" class="href__button"></a> 
        -->
     </div>
 </div>
