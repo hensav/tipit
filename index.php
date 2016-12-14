124d768aea40ef5b212dccde21b8ad98f86dbb86
thetloffline <toomasthetloff@Thetloffline.local>
2016-12-14 13:10:48 +0200

    some UI changes

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require("header.php"); ?>
+
+
+    <div class="welcome">
+        <h1>Empower good people!</h1>
+        <a href="signup.php"><input type="button" value="Don't mind if I do" class="href__button"></a>
+        <a href="learn.php"><input type="button" value="learn more" class="href__button inactive--button"></a>
+        <a href="login.php"><input type="button" value="Login" class="href__button"></a>
+    </div>
+</div>
+
+</body>
+</html>
