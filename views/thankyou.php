ce3c1924c278690cb30e1c5b042e600f6bd17953
henrysavi <hensav@users.noreply.github.com>
2016-12-15 21:27:53 +0200

    changed link to index

--- views/thankyou.php
+++ views/thankyou.php
@@ -2,7 +2,7 @@
 
     <div class="welcome">
         <h1>You are great! Thank you!</h1>
-        <p><a href="signup.php">Don't mind if I do.</a></p>
+        <p><a href="index.php">Tipit</a></p>
         <!--
         <a href="signup.php"><input type="button" value="Don't mind if I do" class="href__button"></a>
         -->
