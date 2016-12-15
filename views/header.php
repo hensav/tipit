3b64815d00f27f44a163dc08c6104f0591150bdc
henrysavi <hensav@users.noreply.github.com>
2016-12-15 13:20:32 +0200

    added jquery to header

--- views/header.php
+++ views/header.php
@@ -4,6 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="./main.css">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,900" rel="stylesheet">
+    <script
+            src="https://code.jquery.com/jquery-3.1.1.min.js"
+            integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+            crossorigin="anonymous">
+    </script>
 </head>
 
 <body>
