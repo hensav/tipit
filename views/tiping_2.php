a07df0dda3679a610d275f16cf5bc19f173cbc3c
vlasn <vlasn@tlu.ee>
2017-01-01 12:48:20 +0200

    Added employee image dummy

--- views/tiping_2.php
+++ views/tiping_2.php
@@ -3,15 +3,16 @@
 
 <div class="employee">
     <h2>Mariann</h2>
+    <img src="http://i4.mirror.co.uk/incoming/article6221356.ece/ALTERNATES/s615b/MAIN-Angry-Seagull.jpg" class="employee-image">
 
 
 <fieldset class="rating">
     <legend></legend>
-    <input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Rocks!"></label>
-    <input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Pretty good"></label>
-    <input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh"></label>
-    <input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Kinda bad"></label>
-    <input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Socks big time"></label>
+    <input type="radio" id="star1" name="mainRating" value="1" /><label for="star1" title="Rocks!"><i class="fa fa-star" aria-hidden="true"></i></label>
+    <input type="radio" id="star2" name="mainRating" value="2" /><label for="star2" title="Pretty good"><i class="fa fa-star" aria-hidden="true"></i></label>
+    <input type="radio" id="star3" name="mainRating" value="3" /><label for="star3" title="Meh"><i class="fa fa-star" aria-hidden="true"></i></label>
+    <input type="radio" id="star4" name="mainRating" value="4" /><label for="star4" title="Kinda bad"><i class="fa fa-star" aria-hidden="true"></i></label>
+    <input type="radio" id="star5" name="mainRating " value="5" /><label for="star5" title="Socks big time"><i class="fa fa-star" aria-hidden="true"></i></label>
 </fieldset>
   
 
