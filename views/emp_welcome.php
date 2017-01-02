928aff3349174389de07c42c0a0a7b34b8d20d41
thetloffline <thetloffline@users.noreply.github.com>
2017-01-03 01:51:49 +0200

    created page for company details

--- views/emp_welcome.php
+++ views/emp_welcome.php
@@ -16,15 +16,14 @@
 
         <form action="upload.php" method="POST" class="signup__form" enctype="multipart/form-data">
             <label class="upload__btn">
- 		    <img src=<?=$employeeImgUrl ?> class="employee-image">
-  		    <input type="file"name="fileToUpload" id="fileToUpload"/>
+ 		    	<img src=<?=$employeeImgUrl ?> class="employee-image">
+  		    	<input type="file"name="fileToUpload" id="fileToUpload"/>
 			</label>
-
- 			<textarea name="empDesciption" placeholder="Hello! This is my good thought of a day. Read it and replace it with yours."><?php echo $empDesciption;?></textarea>
- 			 <input type="submit" value="save" class="form__button" name="submit">
+ 			<textarea name="empDesciption" placeholder="Hello! This is my good thought of the day. Read it and replace it with another one."><?php echo $empDesciption;?></textarea>
+ 			 <input type="submit" value="submit" class="form__button" name="submit">
  		</form>
-       
-
+       <p class="employee--good-code txt-center">This is My Good Code:</p>
+       <p class="good-code txt-center">Ma54r8</p>
     </div>
 </div>
 
