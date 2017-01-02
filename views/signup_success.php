b7601464accf427a8ce96f9ae86f2e8d34a4e06f
henrysavi <hensav@users.noreply.github.com>
2017-01-02 19:24:09 +0200

    small update created page for successful signup

---
+++ views/signup_success.php
@@ -0,0 +1,10 @@
+<?php require("header.php"); ?>
+
+    <div class="welcome">
+        <h1>Thank you! You've successfully signed up.</h1>
+        <p><a href="login.php">Log in</a></p>
+
+    </div>
+    </div>
+
+<?php require("footer.php"); ?>
