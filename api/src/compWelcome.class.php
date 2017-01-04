d2ef1340b8d97e435074d564a12924c8d31dad69
henrysavi <hensav@users.noreply.github.com>
2017-01-04 22:21:03 +0200

    created compWelcome class, query not tested

---
+++ api/src/compWelcome.class.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: henrysavi
+ * Date: 04/01/17
+ * Time: 21:29
+ */
+class compWelcome
+{
+
+    public function __construct($PDO)
+    {
+        $this->conn = $PDO;
+    }
+
+    public function compWelcome($related_user,$trading_name,$email,$address,$description,$opening_hours,$photo_url)
+    {
+        $stmt = $this->conn->prepare("
+            SELECT id, trading_name
+            FROM company
+            WHERE trading_name=:trading_name
+            LIMIT 1
+        ");
+        $stmt->bindParam(':trading_name', $trading_name, PDO::PARAM_INT);
+        $stmt->execute();
+        $checking = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($checking['id'] > 1) {
+            return array(
+                'status'=>'error',
+                'response'=>'Details already in use, please try again');
+        } else {
+            $stmt = $this->conn->prepare("
+                INSERT INTO user VALUES(DEFAULT,:related_user,NULL,:trading_name,:address,NULL,NULL,CURRENT_TIMESTAMP,NULL,:description,:opening_hours,:photo_url)
+            ");
+
+            /*väljad  mis pole kohustuslikud
+
+             * if(empty($)){
+                $=null;
+            }
+
+            */
+
+            $stmt->bindParam(':related_user', $related_user, PDO::PARAM_STR);
+            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+            $stmt->bindParam(':trading_name', $trading_name, PDO::PARAM_INT);
+            $stmt->bindParam(':address', $address, PDO::PARAM_STR);
+            $stmt->bindParam(':description', $description, PDO::PARAM_STR);
+            $stmt->bindParam(':opening_hours', $opening_hours, PDO::PARAM_STR);
+            $stmt->bindParam(':photo_url', $photo_url, PDO::PARAM_STR);
+
+
+            if ($stmt->execute()) {
+                $result =  array(
+                    'status'=>'success',
+                    'response'=>'Company details saved',
+                );
+
+                return $result;
+            }
+        }
+    }
+
+}
