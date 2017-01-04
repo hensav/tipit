2596b15fb3e33fd7c1d1f2ca784f6237a4cdceef
henrysavi <hensav@users.noreply.github.com>
2017-01-04 02:12:33 +0200

    thankyou.php now redirects to company page after 3sec

--- views/thankyou.php
+++ views/thankyou.php
@@ -1,12 +1,17 @@
-<?php require("header.php"); ?> 
+<?php require("header.php"); ?>
 
     <div class="welcome">
         <h1>You are great! Thank you!</h1>
         <p><a href="index.php">Tipit</a></p>
-        <!--
+
+       <!--
         <a href="signup.php"><input type="button" value="Don't mind if I do" class="href__button"></a>
         -->
     </div>
 </div>
+
+
+    <script src="js/redirect.js" type="text/javascript"></script>
+
 
 <?php require("footer.php"); ?>
