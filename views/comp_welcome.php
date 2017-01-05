87d5eba28143aff918af6f4f0672cb65b524e65c
thetloffline <thetloffline@users.noreply.github.com>
2017-01-05 02:41:57 +0200

    modified company.php

--- views/comp_welcome.php
+++ views/comp_welcome.php
@@ -2,8 +2,8 @@
 $employeeName = "Obossum";
 $compImgUrl = "http://i4.mirror.co.uk/incoming/article6221356.ece/ALTERNATES/s615b/MAIN-Angry-Seagull.jpg";
 $employeeId = 4;
-$compDesciption = "";
-$compOpen = "";
+$companyDesciption = "";
+$companyOpen = "";
 
 
 ?>
@@ -23,8 +23,8 @@ $compOpen = "";
 			</label>
         <input class="form__field "type="text" name="company-name" placeholder="What is your comany name?">
         <input class="form__field "type="text" name="company-address" placeholder="What is your comany address?">
-        <textarea name="empDesciption" placeholder="Hello! This is one sentence about us. Read it and replace it with another one."><?php echo $compDesciption;?></textarea>
-        <textarea name="empDesciption" placeholder="Mo—Fri 10—20; Sa—Su 10—00"><?php echo $compOpen;?></textarea>
+        <textarea name="empDesciption" placeholder="Hello! This is one sentence about us. Read it and replace it with another one."><?php echo $companyDesciption;?></textarea>
+        <textarea name="empDesciption" placeholder="Mo—Fri 10—20; Sa—Su 10—00"><?php echo $companyOpen;?></textarea>
         <input type="submit" value="submit" class="form__button" name="submit">
     </form>
 
