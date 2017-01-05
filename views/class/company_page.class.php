d8012824579b22868dd08a425c19e1f4ea7c1077
vlasn <vlasn@tlu.ee>
2017-01-05 19:52:14 +0200

    Created client side methods for company workforce management

--- views/class/company_page.class.php
+++ views/class/company_page.class.php
@@ -8,17 +8,32 @@
  */
 class company_page
 {
-    public static function fetchcompanyData($companyId, $apikey){
-
+    public static function fetchcompanyData($companyId, $apikey)
+    {
         $url="http://naturaalmajand.us/tipit/api/request.php/apikey/$apikey/view/companyView/companyId/$companyId";
         $result = file_get_contents($url);
         return $result;
     }
 
-    public static function fetchCompanyByEmployee($employeeId, $apikey){
-
+    public static function fetchCompanyByEmployee($employeeId, $apikey)
+    {
         $url="http://naturaalmajand.us/tipit/api/request.php/apikey/$apikey/view/fetchCompanyByEmployee/employeeId/$employeeId";
         $result = file_get_contents($url);
         return json_decode($result);
     }
+
+    public static function employeeManagementView($apikey,$companyID)
+    {
+        $url = "http://naturaalmajand.us/tipit/api/request.php/apikey/$apikey/view/employeeManagement/companyId/$companyID";
+        $result = file_get_contents($url);
+        return json_decode($result);
+    }
+
+    public static function addEmployee($apikey,$companyID,$goodCode)
+    {
+        $url = "http://naturaalmajand.us/tipit/api/request.php/apikey/$apikey/view/addEmployee/companyId/$companyID/goodCode/$goodCode";
+        $result = file_get_contents($url);
+        return json_decode($result);
+    }
+
 }
