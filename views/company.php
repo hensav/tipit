f37f361db38660eb998bd2b26c0c23a41dffc295
vlasn <vlasn@tlu.ee>
2017-01-07 21:01:02 +0200

    Built out client-facing company page

--- views/company.php
+++ views/company.php
@@ -1,13 +1,28 @@
 <?php
 
 session_start();
-require("class/compWelcome.class.php");
+require("class/companyPage.class.php");
+/*
+$companyName = "Lendav Taldrik";
+$companyImgUrl = "http://www.northestonia.eu/sites/default/files/styles/large/public/visitestonia_pics/d0b4f1cdca0e3e3aadc1bf3eab9029ba.jpg?itok=4PnXFqtH";
+$companyDescription = "Me ei ole tüüpiline Aasia restoran. Tule ja vaata, milles kogu kära!";
+$companyOpen = "Mo—Fri 10—20; Sa—Su 10—00";
+$companyAddress = "Toomrüütli 666, Tallinn";
 
-$employeeId = $_SESSION['userId'];
+$companyKind = "Restoran";
+$companyName = "Toompea Tina";
+$employeeName = "Jüri Vokiratas";
+$employeeRating = "3.6";
+$companyRating = "0.6";
+*/
+
 $apikey = $_SESSION['apiKey'];
+$companyId = $_GET['company'];
 
+$data = company_page::fetchCompanyData($companyId,$apikey);
+$company = $data[0];
 
-
+$employees = company_page::fetchEmployeesByCompany($companyId,$apikey);
 
 
 //dummydata
@@ -21,30 +36,27 @@ $address = "";
 
 <?php require("header.php"); ?>
 <div class="employee">
-	<h2><?php echo $companyName; ?></h2>
+	<h2><?php echo $company->trading_name; ?></h2>
 	<div class="company__profile">   
-	    <img src=<?=$companyImgUrl?> class="employee-image">
+	    <img src=<?=$company->photo_url?> class="employee-image">
 	    <div class="container__companyAddress">
 	    	<div class="addressPin"><img src="../uploads/emp_thumbnail.png" class="employeeThumbnail"></div>
-	    	<div class="address"><p class="companyAddress"><?php echo $companyAddress;?></p></div>
+	    	<div class="address"><p class="companyAddress"><?php echo $company->address;?></p></div>
 	    </div>
 	    <div class="container__companyAddress">
 	    	<div class="addressPin"></div>
-	    	<div class="address"><p><?php echo $companyOpen; ?></p></div>
+	    	<div class="address"><p><?php echo $company->opening_hours; ?></p></div>
 	    </div>
     </div>
-	    <p class="companyDescription"><?php echo $companyDescription; ?></p>
+	    <p class="companyDescription"><?php echo $company->description; ?></p>
 	</div>
-    <div class="container__employee">
-	    <div class="employeeThumbnail"><img src="../uploads/emp_thumbnail.png" class="employeeThumbnail"></div>
-	    <div class="employeeName"><?php echo $employeeName;?></div>
-	    <div class="employeeRating"><?php echo $employeeRating;?></div>
-	</div>
-	<div class="container__employee">
-	    <div class="employeeThumbnail"><img src="../uploads/emp_thumbnail.png" class="employeeThumbnail"></div>
-	    <div class="employeeName"><?php echo $employeeName;?></div>
-	    <div class="employeeRating"><?php echo $employeeRating;?></div>
-	</div>
+    <?=$employeeSection?>
+    <? foreach($employees as $employee){
+            if($employee->status == "active") {
+                company_page::printEmployeeStatus($employee);
+            }
+        }
+?>
 </div>
 
 
