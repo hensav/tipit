c42e57f59a67a021962f777ca0e97a7a9d76a6c2
henrysavi <hensav@users.noreply.github.com>
2017-01-07 21:05:06 +0200

    error handling needs work

--- views/signup.php
+++ views/signup.php
@@ -11,6 +11,11 @@ $signupPasswordError = "";
 $role_choice = "";
 $phone = "";
 $errorClass = "input-error";
+$nameError = "";
+$phoneError = "";
+$errorMessage = "";
+$error = false;
+
 
 //Default role & post-error select retention
 $rolesel = 'client';
@@ -44,75 +49,104 @@ if (isset($_POST["signupPassword"])) {
         $signupPassword = $_POST["signupPassword"];
     }
 }
+
+
+
+
 //checking if signupEmail and signupPassword have been posted
-if (isset($_POST["signupEmail"]) && isset($_POST["signupPassword"])
-        && !empty($_POST['signupEmail']) && !empty($_POST['signupPassword'])
-        //&& strlen($_POST['signupPassword']>=5)
-    ) {
+if (isset($_POST["signupEmail"]) && isset($_POST["signupPassword"]))
+    //&& strlen($_POST['signupPassword']>=5
+{
 
-    //client registration - only e-mail and passhash are registered.
-    if($_POST['role_choice']=='client'){
+    if (!empty($_POST['signupEmail']) && !empty($_POST['signupPassword']))
+    {
 
-        $result = $clientAuth->registerRequest($url,$_POST['signupEmail'],$_POST['signupPassword'],"0","0",$_POST['role_choice']);
-        $response = json_decode($result);
-        if ($response->status=="success") {
-            header("location: signup_success.php");
-            exit();
-        } else {
-            var_dump($_POST);
-            //client registration error handling here
-            if($response->status == 'error'){
-                // client already exists? Message accessible as string in $response->response
-            }
-        }
 
-    //employe* registration
-    } elseif ($_POST['role_choice'] == 'employee' or $_POST['role_choice'] == 'employer') {
+        //client registration - only e-mail and passhash are registered.
+        if($_POST['role_choice']=='client'){
 
-        $name = $_POST['firstname'].'_'.$_POST['lastname'];
-        if($name == '_'){
-            ///throw error here - name missing
-        } else {
-
-            $result = $clientAuth->registerRequest($url, $_POST['signupEmail'], $_POST['signupPassword'], $_POST['phone'], $name, $_POST['role_choice']);
+            $result = $clientAuth->registerRequest($url,$_POST['signupEmail'],$_POST['signupPassword'],"0","0",$_POST['role_choice']);
             $response = json_decode($result);
-            if ($response->status == "success") {
+            if ($response->status=="success") {
                 header("location: signup_success.php");
                 exit();
-            } if($response->status == 'error'){
-                // employe* already exists? Message accessible as string in $response->response
+            } else {
+                var_dump($_POST);
+                //client registration error handling here
+                if($response->status == 'error'){
+                    // client already exists? Message accessible as string in $response->response
+                }
             }
-        }
 
+            //employe* registration
+        } elseif ($_POST['role_choice'] == 'employee' or $_POST['role_choice'] == 'employer') {
+
+                if (empty ($_POST["firstname"]) or  empty($_POST["lastname"])) {
+                    $error = true;
+                }
+
+                if (empty ($_POST["phone"])) {
+                    $error = true;
+                }
+
+                if (empty ($_POST["phone"])) {
+                    $error = true;
+                }
+
+                if ($error = false) {
+                    $signupEmail = $_POST["signupEmail"];
+                    $signupPassword = $_POST["signuppasword"];
+                    $name = $_POST['firstname'].'_'.$_POST['lastname'];
+                    $phone = $_POST["phone"];
+                    $result = $clientAuth->registerRequest($url, $_POST['signupEmail'], $_POST['signupPassword'], $_POST['phone'], $name, $_POST['role_choice']);
+                    $response = json_decode($result);
+
+                    if ($response->status == "success") {
+                        header("location: signup_success.php");
+                        exit();
+                    } if($response->status == 'error'){
+                    $errorMessage = "Email already registered!";
+                    }
+
+        } else {
+
+                    $errorMessage = "Please fill all fields";
+
+
+            }
+
+
+        }
     }
 
-    } else {
-    //e-mail and password haven't been posted
 }
+
+
+    echo($errorMessage)
+
 ?>
 
 
-<?php require("header.php"); ?> 
+<?php require("header.php"); ?>
 
 
-<div class="signup">
-    <form method="POST" class="signup__form">
-        
-        <select id='sel-role' name="role_choice" class="form__field">
+    <div class="signup">
+        <form method="POST" class="signup__form">
+
+            <select id='sel-role' name="role_choice" class="form__field">
                 <option value="client" <?=$sel_client?>>Client</option>
                 <option value="employee" <?=$sel_employee?>>Employee</option>
                 <option value="employer" <?=$sel_employer?>>Employer</option>
-        </select>
-        <input id='sel-phone' type="text" placeholder="phone number" name="phone" value="" class="form__field field--optional">
-        <input id='sel-email' type="email" placeholder="your e-mail address" name="signupEmail" value="<?=$signupEmail?>" class="form__field <?=$signupEmailError ?>">
-        <input id='sel-fName' type="text" placeholder="first name" name="firstname" value="" class="form__field field--optional">
-        <input id='sel-lName' type="text" placeholder="last name" name="lastname" value="" class="form__field field--optional">
-        <input id='sel-pass' type="password" name="signupPassword" placeholder="password" class="form__field <?=$signupPasswordError ?>">
-        <input id='sel-register' type="submit" value="sign up" class="form__button">
-    </form>
+            </select>
+            <input id='sel-phone' type="text" placeholder="phone number" name="phone" value="" class="form__field <?=$phoneError ?>">
+            <input id='sel-email' type="email" placeholder="your e-mail address" name="signupEmail" value="<?=$signupEmail?>" class="form__field <?=$signupEmailError ?>">
+            <input id='sel-fName' type="text" placeholder="first name" name="firstname" value="" class="form__field <?=$nameError ?>">
+            <input id='sel-lName' type="text" placeholder="last name" name="lastname" value="" class="form__field <?=$nameError ?>">
+            <input id='sel-pass' type="password" name="signupPassword" placeholder="password" class="form__field <?=$signupPasswordError ?>">
+            <input id='sel-register' type="submit" value="sign up" class="form__button">
+        </form>
 
     </div>
-</div>
-<script src="js/login-reg.js"></script>
+    <script src="js/login-reg.js"></script>
 
 <?php require("footer.php"); ?>
